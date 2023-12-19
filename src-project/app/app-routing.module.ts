cb38a4e25dbf249943dcbdbb7d421604fa787be1
Shashankwins <shashankbhardwajwins@gmail.com>
2023-12-19 19:05:37 +0530

    added code

---
+++ src-project/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+const routes: Routes = [
+{path:'', component: LoginComponent},
+{path:'register', component: RegisterComponent},
+{path:'dashboard', component: DashboardComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+export const routingComponents = [LoginComponent, RegisterComponent, DashboardComponent];
